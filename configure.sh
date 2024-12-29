b9736acc74743b2b17164dee2ca39e3e6167c962
mahaskoro <106416530+mahaskoro@users.noreply.github.com>
2024-12-29 23:48:49 +0700

    Update configure.sh
    replace deprecated var

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 ARCH="-march=armv8-a+crypto+sha2+crc"
 CORE="-mtune=cortex-a53"
-OPTI="-Ofast -pthread -fopenmp -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT"
+OPTI="-O3 ffast-math -pthread -fopenmp -flto -fstrict-aliasing -ftree-vectorize -funroll-loops -ffinite-loops -finline-functions -fno-stack-protector -fomit-frame-pointer -fpic -falign-functions=64 -D_REENTRANT"
 
 ./configure CXXFLAGS="$ARCH $CORE $OPTI" CFLAGS="$ARCH $CORE $OPTI" \
 CXX=clang++ CC=clang  LDFLAGS="-Wl,-hugetlbfs-align -fuse-ld=lld"
